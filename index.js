0f3aa8c43cb5f779c3545b1bcc4ef6e047b5ae71
Ilya Altukhov <zompin@ya.ru>
2019-03-03 15:24:30 +0300

    Bodyparser

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const Koa = require('koa');
+const bodyParser = require('koa-bodyparser');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const app = new Koa();
+
+app.use(bodyParser());
 
 app.use(async (ctx) => {
     const body = ctx.request.body || {};
