3cfe4062eb9becf1e355dc06eaa118ff3254ac60
Ilya Altukhov <zompin@ya.ru>
2019-03-03 16:11:35 +0300

    Body fix

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const Koa = require('koa');
-const bodyParser = require('koa-body');
+const bodyParser = require('koa-bodyparser');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const app = new Koa();
-
-console.log(bodyParser())
 
 app.use(bodyParser());
 
 app.use(async (ctx) => {
     console.log(ctx.request)
-    const body = ctx.request.body || {};
-    const payload = body.payload || { ref: '' };
+    const payload = ctx.request.rawBody.payload;
+    const body = JSON.parse(payload)
+    console.log(body)
     const ref = payload.ref.split('/');
     const branch = ref.pop();
     const re = /[^\d-]/g;
