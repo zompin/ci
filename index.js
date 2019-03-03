c07e095b1847a7af18da81ea68da17267c25343e
Ilya Altukhov <zompin@ya.ru>
2019-03-03 15:04:46 +0300

    Init

---
+++ index.js
@@ -0,0 +1,30 @@
+const Koa = require('koa');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const app = new Koa();
+
+app.use(async (ctx) => {
+    const body = ctx.request.body || {};
+    const payload = body.payload || { ref: '' };
+    const ref = payload.ref.split('/');
+    const branch = ref.pop();
+    const re = /[^\d-]/g;
+    let result = '';
+
+    if (ctx.method === 'POST') {
+        try {
+            if (re.test(branch)) {
+                throw new Error();
+            }
+            result = await exec(`cd ../${branch} && git checkout develop && cd hi git pull origin ${branch}`);
+        } catch (e) {
+            result = e;
+        }
+
+        ctx.body = result;
+    } else {
+        ctx.throw(400);
+    }
+});
+
+app.listen(5555);
