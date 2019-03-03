08499531c9f88869cfdf6c59eda731cb9ec78f55
Ilya Altukhov <zompin@ya.ru>
2019-03-03 15:47:40 +0300

    bodyparser => body

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
 const Koa = require('koa');
-const bodyParser = require('koa-bodyparser');
+const bodyParser = require('koa-body');
 const util = require('util');
 const exec = util.promisify(require('child_process').exec);
 const app = new Koa();
 
+console.log(bodyParser())
+
 app.use(bodyParser());
 
 app.use(async (ctx) => {
+    console.log(ctx.request)
     const body = ctx.request.body || {};
     const payload = body.payload || { ref: '' };
     const ref = payload.ref.split('/');
     const branch = ref.pop();
     const re = /[^\d-]/g;
     let result = '';
-    console.log(ctx.request.body)
 
     if (ctx.method === 'POST') {
         try {
