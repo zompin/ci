ab2ab8ac787ddd256b76e1619391b2b62dee3c7b
Ilya Altukhov <zompin@ya.ru>
2019-10-26 09:37:03 +0300

    async exec

--- back/api/mock.js
+++ back/api/mock.js
@@ -1,15 +1,4 @@
-const Router = require('koa-router');
-const path = require('path');
-const fs = require('fs');
-const { exec } = require('child_process');
-const { promisify } = require('util');
-
-const execAsync = promisify(exec);
-
-const router = new Router();
-const sandbox = 'sandbox';
-const deployKeyDir = '/home/zompin/projects/ci/sandbox/deploy';
-const mock = {
+module.exports = {
   ref: 'refs/heads/master',
   before: '445ef7468811c48f2e6091aee492d1b460315291',
   after: '4767a45f1d45cf9b4cd519505d7a29a117525255',
@@ -197,46 +186,3 @@ const mock = {
     ],
   },
 };
-
-function getRep(payload) {
-  const ref = payload.ref.split('/');
-  const { name } = payload.repository;
-  const branch = ref.pop();
-
-  return {
-    name,
-    branch,
-  };
-}
-
-async function hook(payload) {
-  const workDir = path.join(process.cwd(), sandbox);
-  const { name, branch } = getRep(payload);
-  const thread = path.join(workDir, branch, name);
-
-  if (!fs.existsSync(workDir)) {
-    fs.mkdirSync(workDir, { recursive: true });
-  }
-
-  // TODO проверять deployKey
-  if (!fs.existsSync(thread)) {
-    await execAsync(`ssh-agent sh -c '${deployKeyDir}; git clone ${payload.repository.ssh_url} ${thread}'`);
-    await execAsync(`git --work-tree=${thread} --git-dir=${path.join(thread, '.git')} checkout ${branch}`);
-  } else {
-    await execAsync(`git --work-tree=${thread} --git-dir=${path.join(thread, '.git')} reset --hard`);
-    await execAsync(`git --work-tree=${thread} --git-dir=${path.join(thread, '.git')} checkout ${branch}`);
-    await execAsync(`ssh-agent sh -c '${deployKeyDir}; git --work-tree=${thread} --git-dir=${path.join(thread, '.git')} pull'`);
-  }
-}
-
-router.post('/api/v1/webhook', async (ctx) => {
-  ctx.body = 'ok';
-  hook(JSON.parse(ctx.request.body.payload));
-});
-
-router.get('/api/v1/webhook', async (ctx) => {
-  ctx.body = 'ok';
-  hook(mock);
-});
-
-module.exports = router.routes();
--- back/index.js
+++ back/index.js
@@ -1,55 +1,13 @@
 const Koa = require('koa');
 const koaBody = require('koa-body');
-const exec = require('child_process').execSync;
 const { resolve } = require('path');
 const serve = require('koa-static')(resolve(__dirname, 'public'));
 const config = require('config');
 const webhook = require('./api/webhook');
-const prepareRepository = require('./middlewares/prepareRepository');
 
 const port = config.get('port');
 
 const app = new Koa();
-
-function post(ctx) {
-  let rep = { name: '', branch: '' };
-
-  try {
-    rep = getRep(ctx);
-  } catch (e) {}
-
-  const { name, branch, payload } = rep;
-  const result = [];
-  let commands = [
-    `git --work-tree=./${branch}/${name} --git-dir=./${branch}/${name}/.git checkout .`,
-    `git --work-tree=./${branch}/${name} --git-dir=./${branch}/${name}/.git pull origin ${branch}`,
-    `yarn --cwd ./${branch}/${name} prep`,
-  ];
-
-  // try {
-  //   const tmp = require('../commands');
-  //
-  //   if (tmp[name] && tmp[name][branch]) {
-  //     commands = [...commands, ...tmp[name][branch]];
-  //   }
-  // } catch (e) {
-  //   console.warn('"commands.json" not found');
-  // }
-
-  commands.forEach((c) => {
-    let tmp = {};
-
-    try {
-      tmp = exec(c);
-    } catch (e) {
-      tmp = e;
-    }
-
-    result.push(tmp.toString());
-  });
-
-  ctx.body = result;
-}
 
 app.use(koaBody({
   multipart: true,
@@ -58,5 +16,4 @@ app.use(koaBody({
 
 app.use(serve);
 app.use(webhook);
-app.use(prepareRepository);
 app.listen(port);
